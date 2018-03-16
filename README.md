49d4a07edd6719aedd9884756b5c37822e0dbe0a
shulgatyy <shulgatyy@users.noreply.github.com>
2018-03-16 04:06:52 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# apiko-hw01
+
+- [task](https://docs.google.com/presentation/d/1HbhB1xUbhkDQ7mnMV8vTnh0avIFvs30KLIJwpM8YDl4/edit#slide=id.g352cd2f382_0_53)
+- [live "app"](https://shulgatyy.github.io/apiko-hw01/)
---
+++ hw1.js
@@ -0,0 +1,100 @@
+const API = "https://jsonplaceholder.typicode.com";
+
+const parseQuery = ({ search }) =>
+	search
+		.slice(1)
+		.split("&")
+		.reduce((query, entry) => {
+			const [key, value] = entry.split("=");
+			query[key] = value;
+			return query;
+		}, {});
+
+const link = (to = "/", text = "home") => `<a href="${to}">${text}</a>`;
+
+const list = (data, item) => `
+	<ul>
+		${data.map(item).join("")}
+	</ul>
+`;
+
+const homePage = posts => list(
+	posts,
+	({ id, title }) => `<li>${link("?postId=" + id, title)}</li>`
+);
+
+const errorPage = data => `
+	<p>${data}</p>
+	go to ${link()}
+`;
+
+const userPage = data => typeof data === "object"
+	? list(Object.keys(data), k => `<li>${k}: ${userPage(data[k])}</li>`)
+	: data;
+
+const author = data => data
+	? link(`?userId=${data.id}`, data.name)
+	: "(loading...)";
+
+const comments = data => list(data, c => `
+	<li>
+		<h4>${c.name} (${link("mailto:" + c.email, c.email)})</h4>
+		${c.body}
+	</li>
+`);
+
+const detailView = post => `
+	<header>
+		<h2>${post.title}</h2>
+		<span>author: ${author(post.user)}</b></span>
+		<hr />
+	</header>
+	<article> ${post.body} </article>
+	<section>
+		<h3>comments</h3>
+		${post.comments && comments(post.comments)}
+	</section>
+	<footer>${link()}</footer>
+`;
+
+function detailPage(data) {
+	const { id, userId, user, comments } = data;
+	const dataRequests = [];
+	const ajaxIf = (cond, endpoint, cb) => {
+		if (cond) {
+			const promise = getData(endpoint).then(cb);
+			dataRequests.push(promise);
+		}
+	};
+	ajaxIf(!user, `${API}/users/${userId}`, userData => data.user = userData);
+	ajaxIf(!comments, `${API}/posts/${id}/comments`, r => data.comments = r);
+	if (dataRequests.length)
+		Promise.all(dataRequests).then(() => render(detailPage, data));
+	return detailView(data);
+}
+
+router();
+
+function router() {
+	const query = parseQuery(location);
+	let rout = [homePage, `${API}/posts`];
+	if (query.postId) rout = [detailPage, `${API}/posts/${+query.postId}`];
+	if (query.userId) rout = [userPage, `${API}/users/${+query.userId}`];
+	getDataAndRender(...rout);
+}
+
+function getDataAndRender(component, apiEndpoint) {
+	return getData(apiEndpoint)
+		.then(r => render(component, r))
+		.catch(err => render(errorPage, err));
+}
+
+async function getData(apiEndpoint) {
+	const r = await fetch(apiEndpoint);
+	if (r.status === 404) throw "not found";
+	return await r.json();
+}
+
+function render(component, data) {
+	document.getElementById("app").innerHTML = component(data);
+}
